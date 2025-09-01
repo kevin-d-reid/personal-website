38027a926d33d68e19366ffe4daf52c8de681218
Kevin D Reid <114093571+kevin-d-reid@users.noreply.github.com>
2025-09-01 12:26:57 -0700

    Setup Astro config file
    Set website url, disable dev toolbar and trailing slash, and add
    development headers.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://www.kevindreid.com",
+  devToolbar: {
+    enabled: false
+  },
+  server: {
+    headers: {
+      "Content-Security-Policy-Report-Only": "default-src 'none'; frame-ancestors 'none'; style-src 'self'; script-src 'self'; font-src 'self'; img-src 'self'; base-uri 'self'; form-action 'none'",
+      "Content-Type": "text/html; charset=UTF-8",
+      "Cross-Origin-Embedder-Policy-Report-Only": "require-corp",
+	    "Cross-Origin-Opener-Policy-Report-Only": "same-origin",
+	    "Cross-Origin-Resource-Policy": "same-origin",
+      "Permissions-Policy-Report-Only": "accelerometer=(), autoplay=(), camera=(), geolocation=(), gyroscope=(), microphone=(), payment=(), usb=()",
+      "Referrer-Policy": "strict-origin-when-cross-origin",
+      "X-Content-Type-Options": "nosniff",
+      "X-Frame-Options": "DENY",
+      "X-XSS-Protection": "0"
+    }
+  },
+  trailingSlash: "never"
+});
