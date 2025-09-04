0a974959f8f38bb9a9d4fc8e108c69af39761cc8
Kevin D Reid <114093571+kevin-d-reid@users.noreply.github.com>
2025-09-03 17:23:27 -0700

    Add RSS feed with stylesheet

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,23 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET (context) {
+  const posts = (await getCollection("blogposts", ({ data }) => {return data.status === "published"})).sort((a, b) => b.data.date.uploaded.valueOf() - a.data.date.uploaded.valueOf());
+  return rss({
+    title: "Kevin D Reid | Blog",
+    description: "",
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.date.uploaded,
+      description: post.data.description,
+      link: `/blog/${post.id}`,
+    })),
+    customData: `<language>en-ca</language><atom:link href="${context.site}rss.xml" rel="self" type="application/rss+xml" />`,
+    xmlns: {
+      atom: "http://www.w3.org/2005/Atom"
+    },
+    stylesheet: `${context.site}assets/simple-rss.xslt`,
+    trailingSlash: false
+  });
+}
