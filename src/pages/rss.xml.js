88575a9aa60dcfb61f19c4eae1d0dfd51d5992b4
Kevin D Reid <114093571+kevin-d-reid@users.noreply.github.com>
2025-09-17 18:29:05 -0700

    Minor RSS updates
    Changed title and set Atom:link content type to match declared MIME
    type. Application/rss+xml isn't officially recognized by IANA at this
    time.

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,7 +4,7 @@ import { getCollection } from 'astro:content';
 export async function GET (context) {
   const posts = (await getCollection("blogposts", ({ data }) => {return data.status === "published"})).sort((a, b) => b.data.date.uploaded.valueOf() - a.data.date.uploaded.valueOf());
   return rss({
-    title: "Kevin D Reid | Blog",
+    title: "Kevin Reid's Blog",
     description: "",
     site: context.site,
     items: posts.map((post) => ({
@@ -13,7 +13,7 @@ export async function GET (context) {
       description: post.data.description,
       link: `/blog/${post.id}`,
     })),
-    customData: `<language>en-ca</language><atom:link href="${context.site}rss.xml" rel="self" type="application/rss+xml" />`,
+    customData: `<language>en-ca</language><atom:link href="${context.site}rss.xml" rel="self" type="application/xml" />`,
     xmlns: {
       atom: "http://www.w3.org/2005/Atom"
     },
