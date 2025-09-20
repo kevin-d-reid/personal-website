337c73f5514337b0eb319e05ced00418351eacf5
Kevin D Reid <114093571+kevin-d-reid@users.noreply.github.com>
2025-09-20 12:14:19 -0700

    Replace null check script with imported function

--- src/scripts/menu.ts
+++ src/scripts/menu.ts
@@ -1,8 +1,5 @@
-const elemBurger = document.querySelector<HTMLDivElement>(".hamburger");
-const elemNav = document.querySelector<HTMLElement>(".nav-links");
-if (!elemBurger || !elemNav) {
-  throw new ReferenceError("Required element not found.");
-}
-elemBurger.addEventListener("click", () => {
-  elemNav.classList.toggle("expanded");
+import { checkedQuerySelector } from "@scripts/checkedDOMSelectors";
+
+checkedQuerySelector(document,".hamburger").addEventListener("click", () => {
+  checkedQuerySelector(document,".nav-links").classList.toggle("expanded");
 });
