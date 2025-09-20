433fdc8360ddc42a6c2c704979ec2233540e42e7
Kevin D Reid <114093571+kevin-d-reid@users.noreply.github.com>
2025-09-19 21:08:50 -0700

    Convert config to TypeScript, remove comments
    No need for "@ts-check" comment if file extension is TS.

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,6 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://kevindreid.ca",
   trailingSlash: "never",
