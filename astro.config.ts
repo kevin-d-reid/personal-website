f68778bc9d001c36c3b6d4c4e509ece3d44e6bf6
Kevin D Reid <114093571+kevin-d-reid@users.noreply.github.com>
2025-10-03 16:53:25 -0700

    Set build output to file
    Eliminates trailing slash in URLs for Netlify deployments when
    PrettyURLs are used.

--- astro.config.ts
+++ astro.config.ts
@@ -8,6 +8,9 @@ export default defineConfig({
   devToolbar: {
     enabled: false
   },
+  build: {
+    format: "file"
+  },
   server: {
     headers: {
       "Content-Security-Policy-Report-Only": "frame-ancestors 'none'; default-src 'none'; font-src 'self'; img-src 'self'; style-src 'report-sample' 'self'; script-src 'report-sample' 'self'; connect-src 'self'; base-uri 'none'; form-action 'none'",
